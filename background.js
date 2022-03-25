94241cf434c1d79af1ebad46466d58e5d7d7dffa
Emil Kovacev <emilkova@buffalo.edu>
2022-03-25 09:57:14 -0400

    added content to redirect page (needs authorization to add to chrome://)

--- background.js
+++ background.js
@@ -6,24 +6,48 @@ const unblocked = [
 ]
 
 const blocked = [
-    new RegExp('^https?://github.com/?.*$')
+    new RegExp('^https?://github.com/?.*')
 ]
 
-chrome.webNavigation.onCommitted.addListener((details) => {
+// detect whether a site should be blocked
+function scan_url(details) {
     const url = details.url;
-    for (let ws of unblocked) {
-        if (url.match(ws)) {
+    const tabId = details.tabId;
+    for (let regex of unblocked) {
+        if (url.match(regex)) {
             console.log(`allowed ${url}`);
             return;
         }
     }
 
-    for (let ws of blocked) {
-        if (url.match(ws)) {
-            chrome.tabs.update({url: "./redirect.html"});
+    for (let regex of blocked) {
+        if (!url.startsWith('chrome://') && url.match(regex)) {
+            let unblocked_serialized = unblocked.map(_ => _.toString());
+            chrome.scripting.executeScript({
+                target: {tabId: tabId}, 
+                func: setup_redirect,
+                args: [url, unblocked_serialized],
+            });
             console.log(`blocked ${url}`);
             return;
         }
     }
     console.log(`allowed ${url}`);
-});
+}
+
+
+function setup_redirect(url, unblocked) {
+    let content = document.documentElement;
+
+    content.innerHTML = `<p>${url} is blocked</p>`;
+
+    content.innerHTML += '<h1>Allowed pages</h1>';
+    content.innerHTML += '<ul>';
+
+    for (let regex of unblocked) {
+        content.innerHTML += `<li>${regex}</li>`; 
+    }
+    content.innerHTML += '</ul>';
+}
+
+chrome.webNavigation.onCommitted.addListener(scan_url);
--- popup.js
+++ popup.js
@@ -2,10 +2,8 @@ function storeConfig() {
     console.log("Hello world!");
 
     const config = document.getElementById("configFile");
-    if (config != null) {
-        console.log(config);
-        chrome.storage.sync.set({"config_url": config});
-    }
+    console.log(config);
+    chrome.storage.sync.set({"config_url": config});
 }
 
 
