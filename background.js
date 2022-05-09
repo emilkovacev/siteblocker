150ea4d4d78efb51ba939c2a11432a534cd1d037
Emil Kovacev <emilkova@buffalo.edu>
2022-05-09 06:08:05 -0400

    final working version

--- background.js
+++ background.js
@@ -2,52 +2,37 @@
 
 
 const unblocked = [
-    new RegExp('^https?://github.com/makeopensource/?.*')
 ]
 
 const blocked = [
-    new RegExp('^https?://github.com/?.*')
+        new RegExp('^https?://.*youtube.com.*'),
+        new RegExp('^https?://.*discord.com.*'),
+        new RegExp('^https?://.*twitch.tv.*'),
+        new RegExp('^https?://.*reddit.com.*'),
+        new RegExp('^https?://.*twitter.com.*'),
+        new RegExp('^https?://.*stackoverflow.com.*')
 ]
 
 // detect whether a site should be blocked
 function scan_url(details) {
-    const url = details.url;
-    const tabId = details.tabId;
-    for (let regex of unblocked) {
-        if (url.match(regex)) {
-            console.log(`allowed ${url}`);
-            return;
+        const url = details.url;
+        const tabId = details.tabId;
+        for (let regex of unblocked) {
+                if (url.match(regex)) {
+                        console.log(`allowed ${url}`);
+                        return;
+                }
         }
-    }
 
-    for (let regex of blocked) {
-        if (!url.startsWith('chrome://') && url.match(regex)) {
-            let unblocked_serialized = unblocked.map(_ => _.toString());
-            chrome.scripting.executeScript({
-                target: {tabId: tabId}, 
-                func: setup_redirect,
-                args: [url, unblocked_serialized],
-            });
-            console.log(`blocked ${url}`);
-            return;
+        for (let regex of blocked) {
+                if (!url.startsWith('chrome://') && url.match(regex)) {
+                        let unblocked_serialized = unblocked.map(_ => _.toString());
+                        chrome.tabs.update({url: chrome.runtime.getURL("redirect.html")});
+                        console.log(`blocked ${url}`);
+                        return;
+                }
         }
-    }
-    console.log(`allowed ${url}`);
+        console.log(`allowed ${url}`);
 }
 
-
-function setup_redirect(url, unblocked) {
-    let content = document.documentElement;
-
-    content.innerHTML = `<p>${url} is blocked</p>`;
-
-    content.innerHTML += '<h1>Allowed pages</h1>';
-    content.innerHTML += '<ul>';
-
-    for (let regex of unblocked) {
-        content.innerHTML += `<li>${regex}</li>`; 
-    }
-    content.innerHTML += '</ul>';
-}
-
-chrome.webNavigation.onCommitted.addListener(scan_url);
+chrome.webNavigation.onBeforeNavigate.addListener(scan_url);
