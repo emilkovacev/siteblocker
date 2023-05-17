c165c184956e8d9e68f56a4275d27663401e7c28
Emil Kovacev <emilkova@buffalo.edu>
2023-05-16 23:13:38 -0400

    save all files

--- background.js
+++ background.js
@@ -3,28 +3,24 @@
 
 const unblocked = []
 
-const blocked = [
-    new RegExp('youtube.com'),
-    new RegExp('discord.com'),
-    new RegExp('twitch.tv'),
-    new RegExp('reddit.com'),
-    new RegExp('twitter.com'),
-    new RegExp('stackoverflow.com')
-]
-
 // detect whether a site should be blocked
 function scan_url(details) {
-    chrome.storage.sync.get(["blocked"], function (results) {
-        const url = details.url;
-        for (let regex of unblocked) {
+    const url = details.url;
+ 
+    chrome.storage.sync.get(["unblocked"], function (results) {
+        for (let regex of results.unblocked) {
             if (url.match(regex)) {
                 console.log(`allowed ${url}`);
                 return;
             }
         }
+    });
 
+    chrome.storage.sync.get(["blocked"], function (results) {
         for (let b of results.blocked) {
             const regex = new RegExp(b);
+            if (details.parentFrameId != -1) return;
+
             if (!url.startsWith('chrome://') && url.match(regex)) {
                 chrome.tabs.update({url: chrome.runtime.getURL("redirect.html")});
                 console.log(`blocked ${url}`);
